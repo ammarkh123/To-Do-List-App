ccb526c94699dfbca6b3b3cb2ffa9c6c29de2449
ammarkh123 <ammar2632001@gmail.com>
2023-07-09 17:33:57 +0300

    create the main project structure

---
+++ script.js
@@ -0,0 +1,45 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const addBtn = document.getElementById("Add-btn");
+function addTask() {
+  if (inputBox.value == "") {
+    let Alert = alert("You must write something!");
+    Alert = document.style.color = "red";
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    //create a cross icon [x] in a span
+    let span = document.createElement("span");
+    // span.innerHTML = "\u00D7";
+    span.classList.add("x-icon");
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+addBtn.onclick = addTask;
+listContainer.addEventListener(
+  "click",
+  function (event) {
+    //when you click on chicked button
+    if (event.target.tagName === "LI") {
+      event.target.classList.toggle("checked");
+      saveData();
+    }
+    // delete the element when you click on the x-icon.
+    else if (event.target.tagName === "SPAN") {
+      event.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+//save changes although you click refresh button
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
